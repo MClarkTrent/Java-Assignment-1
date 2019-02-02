862173d8dee24c1b10ca5ef874d28d688671b4df
Matt Clark <MattClark@Matts-MacBook-Pro.local>
2019-02-02 14:19:12 -0500

    initial commit

---
+++ GitHub/Java-Assignment-1/JavaFibonacci/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+//Scanner for user input: https://www.programiz.com/java-programming/basic-input-output
+import java.util.Scanner;
+
+public class Main {
+    // Matt Clark (0598737)
+    // This Program will create a fibonacci sequence with a number of terms specified by the user
+    // This will be done both through recursion and through iteration to find the pros and cons of each technique
+    //Last commit to GitHub - Feb 2nd, 2019
+
+    public static void main(String[] args) {
+	// Assign Variables
+        Integer i = 0; //Number of terms user would like to calculate
+        Integer firstNumber = 0; // First term to be added
+        Integer secondNumber = 1; // Secondterm to be added
+
+
+    }
+}
