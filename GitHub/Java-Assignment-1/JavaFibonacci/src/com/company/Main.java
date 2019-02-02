8574a45e3a8a8b140c7e9c44ffac8e1e4075402b
Matt Clark <MattClark@Matts-MacBook-Pro.local>
2019-02-02 15:31:52 -0500

    Added variables, welcome message, input reader

--- GitHub/Java-Assignment-1/JavaFibonacci/src/com/company/Main.java
+++ GitHub/Java-Assignment-1/JavaFibonacci/src/com/company/Main.java
@@ -7,14 +7,28 @@ public class Main {
     // Matt Clark (0598737)
     // This Program will create a fibonacci sequence with a number of terms specified by the user
     // This will be done both through recursion and through iteration to find the pros and cons of each technique
-    //Last commit to GitHub - Feb 2nd, 2019
+    //Last commit to GitHub - Feb 2nd, 2019 @
 
     public static void main(String[] args) {
-	// Assign Variables
-        Integer i = 0; //Number of terms user would like to calculate
+        // Assign Variables
+        Integer terms = 0; //Number of terms user would like to calculate
         Integer firstNumber = 0; // First term to be added
-        Integer secondNumber = 1; // Secondterm to be added
+        Integer secondNumber = 1; // Second term to be added
 
+        // Create Scanner
+        Scanner input = new Scanner(System.in);
+
+        //Welcome users
+        System.out.println("Welcome, this program will create a Fibonacci series based on the number of terms set.");
+        System.out.println("A Fibonacci series of numbers is characterized as the following: 0 1 1 2 3, where the sum of the previous two numbers equal the current number.");
+        System.out.println();   // Blank Line for Spacing
+
+        //Ask for user input
+        System.out.println("How long would you like the series?");
+        //Read input
+        terms = input.nextInt();
+
+        System.out.println(terms);
 
     }
 }
