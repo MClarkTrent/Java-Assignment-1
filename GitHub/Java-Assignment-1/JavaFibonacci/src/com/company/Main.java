60fe445e6ceb421852548db08793e175fd603216
Matt Clark <MattClark@Matts-MacBook-Pro.local>
2019-02-02 21:33:39 -0500

    Fixed issue with adjusting numbers for loop calculation, added iterative funciton

--- GitHub/Java-Assignment-1/JavaFibonacci/src/com/company/Main.java
+++ GitHub/Java-Assignment-1/JavaFibonacci/src/com/company/Main.java
@@ -18,7 +18,8 @@ public class Main
         Integer terms = 0; //Number of terms user would like to calculate
         Integer num1 = 0; // First term to be added
         Integer num2 = 1; // Second term to be added
-        Integer result = 1; //sum of num1 and num2
+        Integer i = 0; //counter for recursive function
+
 
         //Welcome users
         System.out.println("Welcome, this program will create a Fibonacci series based on the number of terms set.");
@@ -29,7 +30,7 @@ public class Main
         terms = getTerms(terms);
 
         //Testing if Iterative works
-        fiboSeriesIte(terms, num1, num2);
+        fiboSeriesRec(terms, num1, num2, i);
 
 
         //End program
@@ -38,41 +39,78 @@ public class Main
     }// End of Main Method
 
 
-    //Recursive Function
-    //public int fiboSeriesRec(int terms, int num1, int num2){
+    //Recursive Function - only call if terms > 2
+    public static int fiboSeriesRec(int terms, int num1, int num2, int i){
+        Integer nextTerm;
 
+        //print only num1
+        if(terms == 1) {
+            System.out.println(num1);
+            //increment counter far enough to stop recursion
+            i = i+5;
+        }
 
-    //}//End of recursive function
+        if (i < 3) {
+            //print first and second term
+            System.out.println(num1);
+            System.out.println(num2);
+            //add 2 to the counter
+            i = i+2;
+        }
+
+        //Begin recursion at terms > 3
+        //Check to see if series is done
+        if( i < terms) {
+            //find and print next term
+            nextTerm = num1 + num2;
+            System.out.println(nextTerm);
+
+            //Adjust num1 and num2
+            num1 = num2;
+            num2 = nextTerm;
+
+            //Add 1 to the counter variable
+            i++;
+
+            //Call the function again (recursion)
+            fiboSeriesRec(terms,num1,num2, i);
+        }
+        //useless return
+        return i;
+    }//End of recursive function
 
 
     //Iterative Function
     public static int fiboSeriesIte(int terms, int num1, int num2){
+        //Set up term to be printed
         Integer nextTerm;
 
+        //Print just the first or firstt 2 if specified
             switch (terms) {
                 case 1:
-                    System.out.println(num1);
+                    System.out.println(num2);
                     break;
                 case 2:
-                    System.out.println(num1);
                     System.out.println(num2);
+                    System.out.println(num1);
                     break;
                 default:
-                    System.out.println(num1);
+                    //print first 2 terms
                     System.out.println(num2);
+                    System.out.println(num1);
+                    //loop to calculate the others
                     for (int i = 2; i<terms; i++) {
                         //print the next term
                         nextTerm = num1 + num2;
                         System.out.println(nextTerm);
 
                         //adjust num1 and num2
-                        num2 = num1;
-                        num1 = nextTerm;
-                    }
-
-            }
+                        num1 = num2;
+                        num2 = nextTerm;
+                    }//end of loop
+            }//end of switch
+        //Useless return
         return terms;
-
     }//End of iterative function
 
 
