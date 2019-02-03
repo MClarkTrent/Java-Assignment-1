73265a312f11085e14828df9a628fc52a69b00e9
Matt Clark <MattClark@Matts-MacBook-Pro.local>
2019-02-02 20:48:56 -0500

    Added fiboSeriesIte function

--- GitHub/Java-Assignment-1/JavaFibonacci/src/com/company/Main.java
+++ GitHub/Java-Assignment-1/JavaFibonacci/src/com/company/Main.java
@@ -6,7 +6,8 @@ import jdk.internal.util.xml.impl.Input;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Main {
+public class Main
+{
     // Matt Clark (0598737)
     // This Program will create a fibonacci sequence with a number of terms specified by the user
     // This will be done both through recursion and through iteration to find the pros and cons of each technique
@@ -15,32 +16,69 @@ public class Main {
     public static void main(String[] args) {
         // Assign Variables
         Integer terms = 0; //Number of terms user would like to calculate
-        Integer firstNumber = 0; // First term to be added
-        Integer secondNumber = 1; // Second term to be added
+        Integer num1 = 0; // First term to be added
+        Integer num2 = 1; // Second term to be added
+        Integer result = 1; //sum of num1 and num2
 
         //Welcome users
         System.out.println("Welcome, this program will create a Fibonacci series based on the number of terms set.");
         System.out.println("A Fibonacci series of numbers is characterized as the following: 0 1 1 2 3, where the sum of the previous two numbers equal the current number.");
         System.out.println();   // Blank Line for Spacing
 
-
-        //Retrieve and test user input from getTerms
+        //Find amount of terms to calculate
         terms = getTerms(terms);
 
-        //Ask for which Calculation method to use
-        
+        //Testing if Iterative works
+        fiboSeriesIte(terms, num1, num2);
 
 
-        //Recursive Function
+        //End program
+        System.out.println("Series Complete!");
+
+    }// End of Main Method
 
 
-        //Iterative Function
+    //Recursive Function
+    //public int fiboSeriesRec(int terms, int num1, int num2){
 
 
-    }
+    //}//End of recursive function
+
+
+    //Iterative Function
+    public static int fiboSeriesIte(int terms, int num1, int num2){
+        Integer nextTerm;
+
+            switch (terms) {
+                case 1:
+                    System.out.println(num1);
+                    break;
+                case 2:
+                    System.out.println(num1);
+                    System.out.println(num2);
+                    break;
+                default:
+                    System.out.println(num1);
+                    System.out.println(num2);
+                    for (int i = 2; i<terms; i++) {
+                        //print the next term
+                        nextTerm = num1 + num2;
+                        System.out.println(nextTerm);
+
+                        //adjust num1 and num2
+                        num2 = num1;
+                        num1 = nextTerm;
+                    }
+
+            }
+        return terms;
+
+    }//End of iterative function
+
 
     //Integer Validation Method
-    public static int getTerms(int terms){
+    public static int getTerms(int terms)
+    {
         // Create Scanner
         Scanner input = new Scanner(System.in);
         //Set terms
@@ -49,21 +87,25 @@ public class Main {
         //test input
         try {
             //ask for input
-            System.out.println("How many terms would you like?");
+            System.out.println("How many terms would you like to calculate?");
             terms = input.nextInt();
         }
         //Handle error
         catch (InputMismatchException e) {
             System.out.println("Invalid Input: Please pick a Number of 1 or greater");
+            System.out.println(" ");
             getTerms(terms);
         }
 
         if (terms < 1){
             System.out.println("Invalid Input: Please pick a Number of 1 or greater");
+            System.out.println(" ");
             getTerms(terms);
         }
+
         //Return Value
         return terms;
     }//End of method
+
 
 }//End of class
