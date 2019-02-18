fb7a665d9b83b99638bc4b77a174a240ccfa5aea
Matt Clark <MattClark@Matts-MacBook-Pro.local>
2019-02-18 12:58:08 -0400

    First commit

---
+++ GitHub/Java-Assignment-2/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    //write your code here
+        GeometricObject gObjectArray [] = new GeometricObject [4];
+        //Complete your code here
+
+
+    }
+
+    private static void printAreaAndPerimeter(GeometricObject gObject) {
+        //Complete your code here
+
+
+    }
+}
+
